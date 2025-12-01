492e2dde01fd65cc51faac9e7320b3abaa4ba54e
edwin2025-cyber <edwin.nunez-infante@ascte.org>
2025-12-01 11:59:09 -0600

    chore: add prebuilt Flutter web output for Vercel

---
+++ im_okay/build/web/firebase-init.js
@@ -0,0 +1,14 @@
+// Firebase Web initialization for ImOkay
+// Generated on 2025-12-01
+
+// eslint-disable-next-line no-unused-vars
+const firebaseConfig = {
+  apiKey: "AIzaSyDC64q3neG5Pwxe_Ecoq-iZCYGO6qtcydo",
+  authDomain: "imokayapp-741a0.firebaseapp.com",
+  databaseURL: "https://imokayapp-741a0-default-rtdb.firebaseio.com",
+  projectId: "imokayapp-741a0",
+  storageBucket: "imokayapp-741a0.firebasestorage.app",
+  messagingSenderId: "566836508326",
+  appId: "1:566836508326:web:bd31b835487758b0d6e667",
+  measurementId: "G-YXCMLFX4DJ"
+};
